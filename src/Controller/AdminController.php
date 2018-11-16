1dfd27c00edb4c6d8e5e6ad39b701d93f9c21d94
Maelan-DPDP <maelan.leborgne@gmail.com>
2018-11-16 14:07:13 +0100

    Presque pret

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Repository\DeplacementRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin")
+ * @Security("has_role('ROLE_ADMIN')")
+ */
 class AdminController extends Controller
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
     public function index()
     {
         return $this->render('admin/accueil.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/stats/nature-deplacements", name="nature-deplacements")
+     */
+    public function natureDeplacements(DeplacementRepository $deplacementRepository) {
+        $statsNbr = $deplacementRepository->getStatsNatureDeplacementsNbr();
+        $statsKilometrage = $deplacementRepository->getStatsNatureDeplacementsKilometrage();
+        return $this->render('graphic/nature-deplacement.html.twig', [
+            'statsNbr' => $statsNbr,
+            'statsKilometrage' => $statsKilometrage
+        ]);
+    }
+    /**
+     * @Route("/stats/jours-util-vehicule", name="jours_util_vehicule")
+     */
+    public function joursUtilVehicule(DeplacementRepository $deplacementRepository) {
+        $stats = $deplacementRepository->getStatsJoursUtilisationByVehicules();
+        dump($stats);
+
+        return $this->render('graphic/jours-utils-vehicule.html.twig', [
+            'stats' => $stats,
         ]);
     }
 }
